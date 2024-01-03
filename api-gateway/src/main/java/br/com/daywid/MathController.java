fe9d94eeef915d96f90cc5dd0e90385a9bef4d62
Daywid <88506180+daywid@users.noreply.github.com>
2024-01-03 15:21:07 -0300

    calculator sum and subtraction operations

--- api-gateway/src/main/java/br/com/daywid/MathController.java
+++ api-gateway/src/main/java/br/com/daywid/MathController.java
@@ -1,40 +1,44 @@
 package br.com.daywid;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.daywid.exceptions.UnsupportedMathOperationException;
+
 @RestController
 public class MathController {
     
-    private final AtomicLong counter = new AtomicLong() ;
-
-    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double sum(
-        @PathVariable(value = "numberOne") String numberOne,
-        @PathVariable(value = "numberTwo") String numberTwo
-        ) throws Exception {
-            
+    @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne") String numberOne,
+                      @PathVariable("numberTwo") String numberTwo) throws Exception { 
         if(!isNumeric(numberOne) || !isNumeric(numberTwo) ){
-            throw new Exception();
+            throw new UnsupportedMathOperationException("Please set a numeric value");
         }
-
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
-    private Double convertToDouble(String strNumber) {
-        if(strNumber == null) return 0D;
-        //
-     
-        
-        return null;
+    @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double subtraction(@PathVariable("numberOne") String numberOne,
+                              @PathVariable("numberTwo") String numberTwo) throws Exception{
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo) ){
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
     }
 
-    private boolean isNumeric(String strNumber) {
-        return false;
+    public static Double convertToDouble(String strNumber) {
+        if(strNumber == null) return 0d;
+        String number = strNumber.replaceAll("," , ".");
+        if (isNumeric(number)) return Double.parseDouble(number);
+        return 1.0d;
+    }
+
+    public static boolean isNumeric(String strNumber) {
+        if(strNumber == null) return false;
+        String number = strNumber.replaceAll("," , ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 
 }
