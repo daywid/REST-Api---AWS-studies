662624f37446b808343691066bf9bd866fbe72f2
Daywid <88506180+daywid@users.noreply.github.com>
2024-01-09 12:06:06 -0300

    Replacing Service Mocks with Repository Access

--- api-gateway/src/main/java/br/com/daywid/services/PersonServices.java
+++ api-gateway/src/main/java/br/com/daywid/services/PersonServices.java
@@ -5,72 +5,80 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.daywid.exceptions.ResourceNotFoundException;
 import br.com.daywid.models.Person;
+import br.com.daywid.repositories.PersonRepository;
 
 @Service
 public class PersonServices {
-    
-    private final AtomicLong counter = new AtomicLong();
+
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    @Autowired
+    PersonRepository repository;
 
     public Person create(Person person) {
 
         logger.info("Creating one person");
         //database access
-        return person;
+        return repository.save(person);
     }
 
     public Person update(Person person) {
 
         logger.info("Updating one person");
         //database access
-        return person;
+        var entity = repository.findById(person.getId())
+            .orElseThrow(() -> new ResourceNotFoundException("No records for this ID") );
+
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+
+        return repository.save(person);
     }
     
     public List<Person> findAll(){
         
         logger.info("Finding all people");
-        
-        List<Person> persons = new ArrayList();
-
-        for (int i = 0; i<8; i++){
-            Person person = mockPerson(i);
-            persons.add(person);
-        }
-        return persons;
+        //database access
+        return repository.findAll();
     }
 
-    public Person findById(String id){
+    public Person findById(Long id){
         
         logger.info("Finding one person");
         
         Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("First");
-        person.setLastName("Last");
-        person.setAddress("city123");
-        person.setGender("gender");
-        return person;
+       //database access
+        return repository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("No records for this ID") );
     }
 
-    public void delete(String id) {
-        
+    public void delete(Long id) {
+
         logger.info("Deleting one person");
+
         //database access
+        var entity = repository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("No records for this ID") );
+        repository.delete(entity);
     }
 
-    private Person mockPerson(int i) {
+    // private Person mockPerson(int i) {
     
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Person name " +i);
-        person.setLastName("Last name " +i);
-        person.setAddress("some address from brazil "+i);
-        person.setGender("male");
-        return person;
+    //     Person person = new Person();
+    //     person.setId(counter.incrementAndGet());
+    //     person.setFirstName("Person name " +i);
+    //     person.setLastName("Last name " +i);
+    //     person.setAddress("some address from brazil "+i);
+    //     person.setGender("male");
+    //     return person;
 
-    }
+    // }
 
 }
